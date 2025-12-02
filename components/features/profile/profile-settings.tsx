e0e24e2d69809d57aea25e8367c70f66bc83b21a
Maks Klemenčič <maks.klemencic6@gmail.com>
2025-12-02 20:55:04 +0100

    adding readme and removing console.log

--- components/features/profile/profile-settings.tsx
+++ components/features/profile/profile-settings.tsx
@@ -84,7 +84,6 @@ const ProfileSettings = React.forwardRef<HTMLDivElement, ProfileSettingsProps>(
     } = useProfilePictureUpload({
       onSuccess: async (profilePictureUrl) => {
         setUploadError(null);
-        console.log('Profile picture upload successful, new URL:', profilePictureUrl);
         queryClient.setQueryData(["user"], (oldData: any) => {
           if (!oldData) return { profilePictureUrl };
           return {
